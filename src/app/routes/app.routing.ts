7baea330aa75bdce450a5d81e9cdf714ae8c7170
christian <christianm.vera@gmail.com>
2019-05-22 17:18:30 -0300

    rutas

--- src/app/routes/app.routing.ts
+++ src/app/routes/app.routing.ts
@@ -4,6 +4,7 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 //importo componentes
+import { AppComponent } from './../app.component';
 import { HomeComponent } from '../components/home/home.component';
 import { FormularioClientesComponent } from "../components/formulario/formulario.component";
 import { NotFoundComponent } from '../components/not-found/not-found.component';
@@ -13,10 +14,8 @@ import { NotFoundComponent } from '../components/not-found/not-found.component';
 //creo array de rutas
 const appRoutes: Routes = 
 [
-    {path:'', component: HomeComponent},
+    {path:'', component: AppComponent},
     {path:'home', component: HomeComponent},
-    {path:'formulario', component: FormularioClientesComponent},
-    {path:'listado', component: ListadoClientesComponent},
     {path: '**', component: NotFoundComponent }
 ];
 
